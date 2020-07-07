5d6d6ea76e6f65b09de5f46c081927c42ebc1fd1
Jackie Curtsinger <jackie_curtsinger@yahoo.com>
2020-07-06 17:54:59 -0700

    added favorite

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,34 +1,43 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView, FlatList } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card, Icon } from 'react-native-elements';
 import { CAMPSITES } from '../shared/campsites';
 import { COMMENTS } from '../shared/comments';
 
-function RenderCampsite({campsite}) {
-   
+function RenderCampsite(props) {
+
+    const { campsite } = props;
     if (campsite) {
         return (
             <Card
-                featuredTitle={campsite.name}
-                image={require('./images/react-lake.jpg')}>
-                <Text style={{margin: 10}}>
-                    {campsite.description}
-                </Text>
-            </Card>
+            featuredTitle={campsite.name}
+            image={require('./images/react-lake.jpg')}>
+            <Text style={{margin: 10}}>
+                {campsite.description}
+            </Text>
+            <Icon
+                name={props.favorite ? 'heart' : 'heart-o'}
+                type='font-awesome'
+                color='#f50'
+                raised
+                reverse
+                onPress={() => props.favorite ? 
+                    console.log('Already set as a favorite') : props.markFavorite()}
+            />
+        </Card>
         );
     }
     return <View />;
 }
 
+function RenderComments({ comments }) {
 
-function RenderComments({comments}) {
-
-    const renderCommentItem = ({item}) => {
+    const renderCommentItem = ({ item }) => {
         return (
-            <View style={{margin: 10}}>
-                <Text style={{fontSize: 14}}>{item.text}</Text>
-                <Text style={{fontSize: 12}}>{item.rating} Stars</Text>
-                <Text style={{fontSize: 12}}>{`-- ${item.author}, ${item.date}`}</Text>
+            <View style={{ margin: 10 }}>
+                <Text style={{ fontSize: 14 }}>{item.text}</Text>
+                <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+                <Text style={{ fontSize: 12 }}>{`-- ${item.author}, ${item.date}`}</Text>
             </View>
         );
     };
@@ -44,18 +53,23 @@ function RenderComments({comments}) {
     );
 }
 class CampsiteInfo extends Component {
-    
+
     constructor(props) {
         super(props);
         this.state = {
             campsites: CAMPSITES,
-            comments: COMMENTS
+            comments: COMMENTS,
+            favorite: false
         };
+    }
+
+    markFavorite() {
+        this.setState({ favorite: true });
     }
 
     static navigationOptions = {
         title: 'Campsite Information'
-    };
+    }
 
     render() {
         const campsiteId = this.props.navigation.getParam('campsiteId');
@@ -63,7 +77,9 @@ class CampsiteInfo extends Component {
         const comments = this.state.comments.filter(comment => comment.campsiteId === campsiteId);
         return (
             <ScrollView>
-                <RenderCampsite campsite={campsite} />
+                <RenderCampsite campsite={campsite}
+                    favorite={this.state.favorite}
+                    markFavorite={() => this.markFavorite()} />
                 <RenderComments comments={comments} />
             </ScrollView>
         );
